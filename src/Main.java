23adc2d6e496c7d0f53f28b938ab717def8b62d7
Nikita <nikitabarkovskij44@gmail.com>
2023-11-07 17:06:13 +0300

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Программа определяет порядок цветов ночью по прошествии количества дней");
+
+        Scanner scanner = new Scanner(System.in);
+        int day = scanner.nextInt();
+
+        if (day % 3 == 0)
+            System.out.println("Порядок цветов на  " + day + " день : GCV");
+        else if (day % 3 == 1)
+            System.out.println("Порядок цветов на  " + day + " день : VGC");
+        else
+            System.out.println("Порядок цветов на  " + day + " день : CVG");
+
+
     }
 }
